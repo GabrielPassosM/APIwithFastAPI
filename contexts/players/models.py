073dbc1b1d5b5bf57459f37c4e83847a0e3639fd
gabmartins <gabrielpassosmartins@gmail.com>
2024-11-06 17:17:37 -0300

    feat(players): player db model

---
+++ contexts/players/models.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+from sqlmodel import SQLModel, Field
+
+from libs.base_types.uuid import BaseUUID
+
+
+class Category(Enum):
+    ARTILHEIROS = "artilheiros"
+    ASSISTENTES = "assistentes"
+    MVPS = "mvps"
+    CARTOES_AMARELOS = "cartoes-amarelos"
+    CARTOES_VERMELHOS = "cartoes-vermelhos"
+
+
+class Player(SQLModel, table=True):
+    id: BaseUUID = Field(default=None, primary_key=True)
+    name: str
+    category: Category
+    quantity: int
+    image_url: str
