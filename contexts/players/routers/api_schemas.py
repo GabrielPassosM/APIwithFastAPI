1b2e185a0ea4cf2300a64d00bf497ec1d96a6405
gabmartins <gabrielpassosmartins@gmail.com>
2024-11-17 18:36:29 -0300

    feat: default image_url on the create api schema

--- contexts/players/routers/api_schemas.py
+++ contexts/players/routers/api_schemas.py
@@ -1,12 +1,19 @@
+from typing import Final
+
 from pydantic import BaseModel, field_validator
 
 from contexts.players.models import PlayerPosition
 
 
+DEFAULT_IMAGE_URL: Final = (
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKu1w7TulWMUKGszjJlb7PDtn0LVSJgGnrog&s"
+)
+
+
 class _PlayerBase(BaseModel):
     name: str
     position: PlayerPosition
-    image_url: str = None
+    image_url: str = DEFAULT_IMAGE_URL
     goals: int = 0
     assists: int = 0
     mvps: int = 0
